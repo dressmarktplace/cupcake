3dbab1a2e1b10315bf8c22dd7554ed549a8fac49
MichelleSilva <dressmarktplace@gmail.com>
2025-10-16 22:00:21 -0300

    Iniciado a pagina produtos

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,11 @@
+import { Header } from "../../components/Header";
+
 export const Products = () => {
     return (
-        <main className="min-h-dvh grid place-items-center bg-slate-50">
+        <main className="bg-pink-200 min-h-screen">
+            <Header />
             <h1 className="text-3xl font-bold text-slate-800">
+                
                 Produtos
             </h1>
         </main>
