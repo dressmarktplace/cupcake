90f48f96ab765a348cfb8102cbb15c50a0c60f17
Michelle <dressmarktplace@gmail.com>
2025-10-17 02:09:18 -0300

    Feito o html e inserido as imagens dos produtos

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -4,10 +4,31 @@ export const Products = () => {
     return (
         <main className="bg-pink-200 min-h-screen">
             <Header />
-            <h1 className="text-3xl font-bold text-slate-800">
-                
-                Produtos
-            </h1>
+
+         <article className="px-4 mx-auto flex inline-grid grid-cols-3 items-center py-15 px-60">
+
+                <img src="./foto1.png" alt="Foto 1" className="lg:max-w-3xs mb-10" />
+                <img src="./foto2.png" alt="Foto 2" className="lg:max-w-3xs mb-10" />
+                <img src="./foto3.png" alt="Foto 3" className="lg:max-w-3xs mb-10" />
+                <img src="./foto4.png" alt="Foto 4" className="lg:max-w-3xs mb-10" />
+                <img src="./foto5.png" alt="Foto 5" className="lg:max-w-3xs mb-10" />
+                <img src="./foto6.png" alt="Foto 6" className="lg:max-w-3xs mb-10" />
+
+                <p className="text-slate-800 text-center">
+                      Baunilha
+                  15,00
+                 Baunilha com cereja
+                  19,00
+                  Morango
+                  15,00
+                 Chocolate com morango
+                   15,00
+                  Chocolate
+                   15,00
+                 Doce de leite com Chocolate
+                   15,00
+                </p>
+            </article>
         </main>
     );
 }
