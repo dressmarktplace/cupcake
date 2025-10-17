3f802114cd32aa7454cbddb32ae0d6a9adbe6713
Michelle <dressmarktplace@gmail.com>
2025-10-17 03:50:13 -0300

    Criado as legendas debaixo das fotos

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -5,29 +5,37 @@ export const Products = () => {
         <main className="bg-pink-200 min-h-screen">
             <Header />
 
-         <article className="px-4 mx-auto flex inline-grid grid-cols-3 items-center py-15 px-60">
-
-                <img src="./foto1.png" alt="Foto 1" className="lg:max-w-3xs mb-10" />
-                <img src="./foto2.png" alt="Foto 2" className="lg:max-w-3xs mb-10" />
-                <img src="./foto3.png" alt="Foto 3" className="lg:max-w-3xs mb-10" />
-                <img src="./foto4.png" alt="Foto 4" className="lg:max-w-3xs mb-10" />
-                <img src="./foto5.png" alt="Foto 5" className="lg:max-w-3xs mb-10" />
-                <img src="./foto6.png" alt="Foto 6" className="lg:max-w-3xs mb-10" />
-
-                <p className="text-slate-800 text-center">
-                      Baunilha
-                  15,00
-                 Baunilha com cereja
-                  19,00
-                  Morango
-                  15,00
-                 Chocolate com morango
-                   15,00
-                  Chocolate
-                   15,00
-                 Doce de leite com Chocolate
-                   15,00
-                </p>
+          
+          <article className="text-center p-4 gap-8 grid gride-cols-3 max-w-4xl mx-auto">  
+            
+               <figure>
+                
+                    <img src="./foto1.png" alt="Foto 1" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                        Baunilha
+                        15,00                   
+                    <img src="./foto2.png" alt="Foto 2" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                    Baunilha com cereja
+                     19,00
+                    <img src="./foto3.png" alt="Foto 3" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                    Morango 
+                    15,00
+                    <img src="./foto4.png" alt="Foto 4" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                    Chocolate com morango 
+                    20,00
+                    <img src="./foto5.png" alt="Foto 5" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                    Chocolate
+                     15,00
+                    <img src="./foto6.png" alt="Foto 6" className="max-w-4xl mx-auto" />
+                     <figcaption className="mt-2 text-center text-sm text-black-800"/>
+                    Doce de leite com Chocolate
+                     20,00
+                
+                </figure>
             </article>
         </main>
     );
