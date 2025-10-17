27c861e6cf64fb8439475c38c8c114630d7ab9cb
MichelleSilva <dressmarktplace@gmail.com>
2025-10-16 21:10:15 -0300

    Estilização da página com o texto principal e imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import Products from "./Pages/Products.jsx";
-import Contact from "./Pages/Contact.jsx";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { Contact } from "./pages/Contact";
 
 const App = () => {
-return (
-<>
-
-<nav className="flex gap-4 p-4 border-b">
-<Link to="/" className="hover:underline">Home</Link>
-<Link to="/Products" className="hover:underline">Produtos</Link>
-<Link to="/Contact" className="hover:underline">Contato</Link>
-</nav>
-
-<Routes>
-<Route path="/" element={<Home/>} />
-<Route path="/products" element={<Products/>} />
-<Route path="/contact" element={<Contact/>} />
-</Routes>
-</>
-);
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,17 @@
 export const Home = () => {
     return (
-        <main className="min-h-dvh grid place-items-center bg-slate-50">
-            <h1 className="text-3xl font-bold text-slate-800">
-                Home
-            </h1>
+        <main className="bg-pink-200 min-h-screen">
+            <article className="lg:max-w-md mx-auto flex flex-col items-center text-center py-12 px-8">
+                <img src="./pagina1foto.png" alt="Foto principal do cupcake" className="lg:max-w-3xs mb-10" />
+
+                <p className="text-slate-800 text-lg">
+                    Olá, amantes de coisas boas!
+                    Você está tendo aquele dia que precisa de um pequeno boost de felicidade? Ou talvez esteja planejando uma reunião especial e quer impressionar sem esforço?
+                    Apresentamos a solução perfeita: nossos Cupcakes Artesanais!
+                    Cada um é feito com ingredientes de primeira, muito carinho e uma pitada extra de magia. Não são só bolinhos; são momentos em forma de doce.
+                    Imagine a massa fofinha, a cobertura cremosa que derrete na boca e o sabor inesquecível que te transporta para um lugar feliz.
+                </p>
+            </article>
         </main>
     );
 }
